939c6ec85b130eeeca808ed6d23e8b5661fcc1d0
QiaoQiao <476920650@qq.com>
2018-11-01 11:26:01 +0800

    添加项目文件。

---
+++ TabbedPageSamples/TabbedPageSamples.Android/Renderers/TabbedPageRenderer.cs
@@ -0,0 +1,57 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Support.Design.Internal;
+using Android.Support.Design.Widget;
+using System;
+using TabbedPageSamples.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(TabbedPageRenderer))]
+namespace TabbedPageSamples.Droid.Renderers {
+    public class TabbedPageRenderer : Xamarin.Forms.Platform.Android.AppCompat.TabbedPageRenderer {
+        public TabbedPageRenderer(Context context) : base(context) {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e) {
+            base.OnElementChanged(e);
+            var activity = (FormsAppCompatActivity)Context;
+
+            if (e.NewElement != null) {
+                var relativeLayout = this.GetChildAt(0) as Android.Widget.RelativeLayout;
+                if (relativeLayout != null) {
+                    var bottomNarView = relativeLayout.GetChildAt(1).JavaCast<BottomNavigationView>();
+                    bottomNarView.SetShiftMode(false, false);
+                }
+            }
+        }
+    }
+    public static class BottomNavigationViewUtils {
+        public static void SetShiftMode(this BottomNavigationView bottomNavigationView, bool enableShiftMode, bool enableItemShiftMode) {
+            try {
+                var menuView = bottomNavigationView.GetChildAt(0) as BottomNavigationMenuView;
+                if (menuView == null) {
+                    return;
+                }
+                var shiftMode = menuView.Class.GetDeclaredField("mShiftingMode");
+
+                shiftMode.Accessible = true;
+                shiftMode.SetBoolean(menuView, enableShiftMode);
+                shiftMode.Accessible = false;
+                shiftMode.Dispose();
+                for (int i = 0; i < menuView.ChildCount; i++) {
+                    var item = menuView.GetChildAt(i) as BottomNavigationItemView;
+                    if (item == null)
+                        continue;
+                    item.SetShiftingMode(enableItemShiftMode);
+                    item.SetChecked(item.ItemData.IsChecked);
+                }
+                menuView.UpdateMenuView();
+            }
+            catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine($"Unable to set shift mode: {ex}");
+            }
+        }
+    }
+}
---
+++ TabbedPageSamples/TabbedPageSamples/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace TabbedPageSamples {
+    public partial class App : Xamarin.Forms.Application {
+        public App() {
+            InitializeComponent();
+            var tabbedPage = new Xamarin.Forms.TabbedPage();
+            tabbedPage.On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            tabbedPage.BarBackgroundColor = Color.White;
+            tabbedPage.Children.Add(new MainPage() { Title = "标题", Icon = "Icon.png" });
+            tabbedPage.Children.Add(new MainPage() { Title = "标题", Icon = "Icon.png" });
+            tabbedPage.Children.Add(new MainPage() { Title = "标题", Icon = "Icon.png" });
+            tabbedPage.Children.Add(new MainPage() { Title = "标题", Icon = "Icon.png" });
+            tabbedPage.Children.Add(new MainPage() { Title = "标题", Icon = "Icon.png" });
+            MainPage = tabbedPage;
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
